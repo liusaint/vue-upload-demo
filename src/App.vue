56f04c7078073f4accdde4db077b17f68afee599
Runner <841766635@qq.com>
2017-08-31 11:21:17 +0800

    引入less

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,14 @@
 </template>
 
 <script>
+
+
 export default {
   name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="less"> 
+  @import './css/index.less';
 </style>
