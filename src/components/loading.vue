cc67054078468d8d734bf7f692faa204742cf23c
Runner <841766635@qq.com>
2017-09-07 00:03:23 +0800

    loading组件

---
+++ src/components/loading.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="loading-box" style="position: fixed;">
+	<img class="middle" src="/image/loading/small_load.gif">
+	</div>
+</template>
+<script>
+	export default {
+
+	}
+</script>
+<style>
+	.loading-box{
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		background: rgba(0,0,0,.3);
+		color: #fff;
+		box-shadow: 0 0 5px rgba(0,0,0,.3);
+		padding: 0;
+		text-shadow: 0 0 2px #000;
+		z-index: 999999;
+		top: 0;
+	}
+	.loading-box .middle {
+		position: absolute;
+		width: 70px;
+		height: 70px;
+		border-radius: 15px;
+		left: 50%;
+		top: 50%;
+		margin: -35px 0 0 -35px;
+	}
+</style>
